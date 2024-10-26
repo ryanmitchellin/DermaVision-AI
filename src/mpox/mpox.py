4317d81591374cc94fc483098d962872cdf3b8c9
ryanmitchellin <ryanvincent999@gmail.com>
2024-10-26 11:51:22 -0700

    test import

---
+++ src/mpox/mpox.py
@@ -0,0 +1,6 @@
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("joydippaul/mpox-skin-lesion-dataset-version-20-msld-v20")
+
+# print("Path to dataset files:", path)
