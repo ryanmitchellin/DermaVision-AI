9aa9b0e31ba4711a5a30f498cb22b85f88fc8053
VerrillAng <vaa13@sfu.ca>
2024-11-25 22:29:14 -0800

    rename

---
+++ src/stages/2_pca.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+import os
+import joblib
+
+source = './src/stages/augmented_data'
+
+data = []
+labels = []
+format = ('.jpg', '.jpeg')
+
+for severity_level in os.listdir(source):
+    severity_path = os.path.join(source, severity_level)
+    
+    if os.path.isdir(severity_path):
+        print(f"Reading {severity_level}...")
+        
+        for img_name in os.listdir(severity_path):
+            img_path = os.path.join(severity_path, img_name)
+            
+            if img_name.lower().endswith(format):
+                image = cv2.imread(img_path)
+
+                if image is None:
+                    continue
+
+                image = cv2.resize(image, (128,128))
+                data.append(image.flatten())
+                labels.append(severity_level)
+            else:
+                continue
+
+output_path = './src/stages/pca_output.csv'
+data = np.array(data)
+print("Applying PCA for dimensionality reduction...")
+pca = PCA(n_components=50) # Change here for the number of feature extracted 
+features_reduction = pca.fit_transform(data)
+
+severity_level_information = [
+    [labels] + list(features) for labels, features in zip(labels, features_reduction)
+]
+
+cols = ['Severity'] + [f'Principal_Components_{i}' for i in range(features_reduction.shape[1])]
+df = pd.DataFrame(severity_level_information, columns=cols)
+df.to_csv(output_path, index=False)
+print(f"Features saved to {output_path}")
+
+# Save the PCA Model
+joblib.dump(pca, './src/stages/models/2_pca_model.pkl')
+print("Final model saved as 2_pca_model.pkl")
---
+++ src/stages/draft/4_fourier_testing.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+import pandas as pd
+import os
+from sklearn.decomposition import PCA
+import joblib
+
+source_directory = './src/stages/augmented_data'
+
+transformed_features = [] 
+classes = [] 
+
+for severity_level in os.listdir(source_directory):
+    severity_path = os.path.join(source_directory, severity_level)
+
+    if os.path.isdir(severity_path):
+        print(f"Extracting {severity_level}")
+
+        images = os.listdir(severity_path)
+        print(f"Found {len(images)} images in {severity_level}")
+        
+        for img in images:
+            img_path = os.path.join(severity_path, img)
+            print(f"Processing image: {img}")
+
+            # Ensure the file is an image
+            if not img.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+                print(f"Skipping non-image file: {img}")
+                continue
+
+            # Read and process the image
+            image = cv2.imread(img_path)
+            if image is None:
+                print(f"Failed to load image: {img_path}")
+                continue
+
+            # Resize for consistent processing
+            image = cv2.resize(image, (128, 128))
+            
+            channels = cv2.split(image)
+            level_features = []
+            for channel in channels:
+                fourierTransform = np.fft.fft2(channel)
+                fourierShift = np.fft.fftshift(fourierTransform)
+                magnitude = 20 * np.log(np.abs(fourierShift) + 1e-10)
+                level_features.extend(magnitude.flatten())
+            
+    
+            transformed_features.append(level_features) 
+            classes.append(severity_level) 
+
+
+transformed_features = np.array(transformed_features)
+print("Applying PCA for dimensionality reduction...")
+pca = PCA(n_components=50)
+features_reduction = pca.fit_transform(transformed_features)
+
+severity_level_information = [
+    [severity] + list(features)
+    for severity, features in zip(classes, features_reduction)
+]
+
+cols = ['Severity'] + [f'Principal_Components_{i}' for i in range(features_reduction.shape[1])]
+output_csv = './src/stages/fourier_pca_output.csv'
+df = pd.DataFrame(severity_level_information, columns=cols)
+df.to_csv(output_csv, index=False)
+print(f"Features saved to {output_csv}")
+
+# Save the PCA Model
+joblib.dump(pca, './src/stages/draft/4_fourier_pca_model.pkl')
+print("Final model saved as 4_fourier_pca_model.pkl")
